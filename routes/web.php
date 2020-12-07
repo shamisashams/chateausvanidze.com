a3cf68d307878cb3d71e23a3dbaeb231a9956ad2
dealense7 <60123889+dealense7@users.noreply.github.com>
2020-12-07 12:04:54 +0400

    admin base changed

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    
     return view('welcome');
 });
+
 Route::prefix('test')->group(function (){
     Route::get('/', function () {
         dd('da');
