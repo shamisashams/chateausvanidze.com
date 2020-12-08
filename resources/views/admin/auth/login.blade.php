1e1a6c86a233f529781a470c135fad85359d03f4
makhatadze <45604845+makhatadze@users.noreply.github.com>
2020-12-08 10:34:19 +0400

    Show login form validation errors.

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -9,22 +9,33 @@
                 Login Form
             </h4>
             {!! Form::open(['url' => route('login'),'method' =>'post']) !!}
-                <div class="form-group">
-                    {{ Form::label('email', 'Username', []) }}
-                    {{ Form::email('email', '', ['class' => 'form-control', 'no','placeholder'=>'Enter your username']) }}
-                </div>
-            <div class="form-group">
-                {{ Form::label('password', 'Password', []) }}
-                {{ Form::password('password',  ['class' => 'form-control', 'no','placeholder'=>'Enter your password']) }}
-            </div>
-                <div class="buttons-w">
-                    <button class="btn btn-primary">Log me in</button>
-                    <div class="form-check-inline">
-                        <label class="form-check-label"><input class="form-check-input" name="remember_me" type="checkbox">Remember Me</label>
-                    </div>
-                </div>
-            {!! Form::close() !!}
+            <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+            {{ Form::label('email', 'Username', []) }}
+            {{ Form::email('email', '', ['class' => 'form-control', 'no','placeholder'=>'Enter your username']) }}
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    {{ $errors->first('email') }}
+                </span>
+            @endif
         </div>
+        <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+            {{ Form::label('password', 'Password', []) }}
+            {{ Form::password('password',  ['class' => 'form-control', 'no','placeholder'=>'Enter your password']) }}
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    {{ $errors->first('password') }}
+                </span>
+            @endif
+        </div>
+        <div class="buttons-w">
+            <button class="btn btn-primary">Log me in</button>
+{{--            <div class="form-check-inline">--}}
+{{--                <label class="form-check-label"><input class="form-check-input" name="remember_me" type="checkbox">Remember--}}
+{{--                    Me</label>--}}
+{{--            </div>--}}
+        </div>
+        {!! Form::close() !!}
+    </div>
     </div>
 @endsection
 
