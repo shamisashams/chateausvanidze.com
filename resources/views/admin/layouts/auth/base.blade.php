263caa3b6d671e96f65c5a75188ee116c7c300f5
makhatadze <45604845+makhatadze@users.noreply.github.com>
2020-12-08 10:32:54 +0400

    Import custom css in auth layout.

--- resources/views/admin/layouts/auth/base.blade.php
+++ resources/views/admin/layouts/auth/base.blade.php
@@ -19,14 +19,19 @@
     <link href="{{asset('../adm/bower_components/select2/dist/css/select2.min.css')}}" rel="stylesheet">
     <link href="{{asset('../adm/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
     <link href="{{asset('../adm/bower_components/dropzone/dist/dropzone.css')}}" rel="stylesheet">
-    <link href="{{asset('../adm/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('../adm/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}"
+          rel="stylesheet">
     <link href="{{asset('../adm/bower_components/fullcalendar/dist/fullcalendar.min.css')}}" rel="stylesheet">
     <link href="{{asset('../adm/bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css')}}" rel="stylesheet">
     <link href="{{asset('../adm/bower_components/slick-carousel/slick/slick.css')}}" rel="stylesheet">
     <link href="{{asset('../adm/css/main.css?version=4.4.0')}}" rel="stylesheet">
     <link href="{{asset('../adm/css/fonts.css')}}" rel="stylesheet">
+
+    {{--         Import Custom css.--}}
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
 </head>
-<body class="auth-wrapper" >
-    @yield('body')
+<body class="auth-wrapper">
+@yield('body')
 </body>
 </html>
