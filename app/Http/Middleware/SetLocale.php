fb6289cf49709c7546144643808f53800eaa568b
makhatadze <vitomaxatadze@gmail.com>
2020-12-17 10:51:39 +0400

    If locale not exist or it's wrong after redirect route with default localization abbreviation.

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,28 +4,49 @@ namespace App\Http\Middleware;
 
 use App\Models\Localization;
 use Closure;
-use function PHPUnit\Framework\throwException;
+use Illuminate\Http\RedirectResponse;
+
 
 class SetLocale
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $locale = $request->segment(1);
-        $localization = Localization::where('abbreviation',$locale)->first();
-         if ($localization == null) {
-             throwException('Localization is wrong.');
-         }
-         if (!$localization->status) {
-             throwException('Localization is disabled..');
-         }
+        $segments = $request->segments();
+
+        $localization = Localization::where('abbreviation', $locale)->first();
+        $defaultLocale = Localization::where('default', true)->first();
+
+        if ($localization == null) {
+            array_unshift($segments, $defaultLocale->abbreviation);
+            return $this->redirectTo($segments);
+        }
+        if (!$localization->status) {
+            array_shift($segments);
+            return $this->redirectTo($segments);
+        }
         app()->setLocale($locale);
         return $next($request);
+    }
+
+
+    /**
+     * Redirect to default localization.
+     *
+     * @param array $segments
+     *
+     * @return RedirectResponse
+     */
+    protected function redirectTo(array $segments)
+    {
+        return redirect()->to(implode('/', $segments));
     }
 }
