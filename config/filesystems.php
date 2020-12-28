44e700d60edbd536a473ae07bb1e522dd9a98284
makhatadze <vitomaxatadze@gmail.com>
2020-12-28 16:17:26 +0400

    Change link directory.

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public_html'),
+        base_path('/public_html/storage') => storage_path('app/public'),
     ],
 
 ];
