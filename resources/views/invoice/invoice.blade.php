be47d3685f05164fc3e841209f17902020836d20
makhatadze <45604845+makhatadze@users.noreply.github.com>
2020-12-29 18:20:55 +0400

    Change fonts.

--- resources/views/invoice/invoice.blade.php
+++ resources/views/invoice/invoice.blade.php
@@ -13,6 +13,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <style>
+        body {
+            font-family: DejaVu Sans;
+        }
+
         .text-right {
             text-align: right;
         }
