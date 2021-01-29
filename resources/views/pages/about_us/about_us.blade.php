4ed1a80a5674f0d1aa5feb344573a47c4e85e30c
makhatadze <vitomaxatadze@gmail.com>
2021-01-29 16:57:36 +0400

    Remove contact information from about page.

--- resources/views/pages/about_us/about_us.blade.php
+++ resources/views/pages/about_us/about_us.blade.php
@@ -1,12 +1,3 @@
-<?php
-
-use App\Models\Setting;
-
-$address = Setting::where('key', 'address')->first();
-$phone = Setting::where('key', 'phone')->first();
-$contactEmail = Setting::where('key', 'contact_email')->first();
-?>
-
 @extends('layouts.base')
 @section('content')
     <main>
@@ -37,26 +28,6 @@ $contactEmail = Setting::where('key', 'contact_email')->first();
                     <p class="r">{{(count($page->availableLanguage) > 0) ?  $page->availableLanguage[0]->content : ''}}</p>
                     <p class="g">{{(count($page->availableLanguage) > 0) ?  $page->availableLanguage[0]->content_2 : ''}}</p>
 
-                        <div class="contact-info">
-                            <a href="#" class="info">
-                                <div class="icon">
-                                    <img src="{{asset('../img/icons/location.svg')}}">
-                                </div>
-                                <p>{!! count($address->availableLanguage) > 0 ? $address->availableLanguage[0]->value : '' !!}  </p>
-                            </a>
-                            <a href="#" class="info">
-                                <div class="icon">
-                                    <img src="{{asset('../img/icons/call.svg')}}">
-                                </div>
-                                <p>{!! count($phone->availableLanguage) > 0 ? $phone->availableLanguage[0]->value : '' !!}  </p>
-                            </a>
-                            <a href="#" class="info">
-                                <div class="icon">
-                                    <img src="{{asset('../img/icons/email.svg')}}">
-                                </div>
-                                <p>{!! count($contactEmail->availableLanguage) > 0 ? $contactEmail->availableLanguage[0]->value : '' !!}  </p>
-                            </a>
-                        </div>
                     </div>
                 </div>
                 <div class="fx bottom">
