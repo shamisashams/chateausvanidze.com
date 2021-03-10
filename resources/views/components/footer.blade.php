115af7d3d3c906f7689f2b1d92509f200a54a087
makhatadze <vitomaxatadze@gmail.com>
2021-03-10 15:08:24 +0400

    Add copyright.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -128,101 +128,15 @@ $instagram = Setting::where('key', 'instagram')->first();
 
             </div>
         </div>
-
-    </div>
-
-    {{--    <div class="footer__bottom">--}}
-    {{--        <div class="container">--}}
-
-    {{--            <p>--}}
-    {{--                <img src="{{asset('../img/icons/svg-phone.svg')}}" alt="">--}}
-    {{--                {!! count($phone->availableLanguage) > 0 ? $phone->availableLanguage[0]->value : '' !!}--}}
-    {{--            </p>--}}
-
-    {{--            <p>--}}
-    {{--                <img src="{{asset('../img/icons/svg-mail.svg')}}" alt="">--}}
-    {{--                {!! count($contactEmail->availableLanguage) > 0 ? $contactEmail->availableLanguage[0]->value : '' !!}--}}
-    {{--            </p>--}}
-
-    {{--            <div class="language-box">--}}
-    {{--                <div class="language-menu">--}}
-    {{--                    @if(isset($languages['current']))--}}
-    {{--                        <a href="">--}}
-    {{--                            <img class="flag" src="/adm/img/flags-icons/{{$languages['current']['img']}}">--}}
-    {{--                            {{strtoupper($languages['current']['abbreviation'])}}--}}
-
-    {{--                        </a>--}}
-    {{--                        @if(count($languages['data']) > 0)--}}
-
-    {{--                            @foreach($languages['data'] as $data)--}}
-    {{--                                <a href="{{$data['url']}}">--}}
-    {{--                                    <img src="/adm/img/flags-icons/{{$data['img']}}" alt="">--}}
-    {{--                                    {{strtoupper($data['abbreviation'])}}--}}
-
-    {{--                                </a>--}}
-    {{--                            @endforeach--}}
-    {{--                        @endif--}}
-    {{--                    @endif--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-
-    {{--            <p>--}}
-    {{--                <img src="{{asset('../img/icons/svg-pin.svg')}}" alt="">--}}
-    {{--                {!! count($address->availableLanguage) > 0 ? $address->availableLanguage[0]->value : '' !!}--}}
-    {{--            </p>--}}
-
-
-    {{--            <div class="soc-links">--}}
-    {{--                <a href="{{count($facebook->availableLanguage) > 0 ? $facebook->availableLanguage[0]->value : ''}}" target="_blank">--}}
-    {{--                    <svg id="Group_7" data-name="Group 7" xmlns="http://www.w3.org/2000/svg" width="6.427" height="12" viewBox="0 0 6.427 12">--}}
-    {{--                        <path id="Icon_awesome-facebook-f" data-name="Icon awesome-facebook-f" d="M7.615,6.75l.333-2.172H5.865V3.169A1.086,1.086,0,0,1,7.089,2h.947V.147A11.553,11.553,0,0,0,6.355,0,2.651,2.651,0,0,0,3.517,2.923V4.578H1.609V6.75H3.517V12H5.865V6.75Z" transform="translate(-1.609)"/>--}}
-    {{--                    </svg>--}}
-    {{--                </a>--}}
-
-    {{--                <a href="{{count($twitter->availableLanguage) > 0 ? $twitter->availableLanguage[0]->value : ''}}" target="_blank">--}}
-    {{--                    <svg id="Group_6" data-name="Group 6" xmlns="http://www.w3.org/2000/svg" width="10.998" height="8.933" viewBox="0 0 10.998 8.933">--}}
-    {{--                        <path id="Icon_awesome-twitter" data-name="Icon awesome-twitter" d="M9.868,5.607c.007.1.007.2.007.293a6.369,6.369,0,0,1-6.413,6.413A6.37,6.37,0,0,1,0,11.3a4.663,4.663,0,0,0,.544.028,4.514,4.514,0,0,0,2.8-.963A2.258,2.258,0,0,1,1.235,8.8a2.843,2.843,0,0,0,.426.035,2.384,2.384,0,0,0,.593-.077A2.254,2.254,0,0,1,.447,6.549V6.521a2.27,2.27,0,0,0,1.019.286,2.257,2.257,0,0,1-.7-3.015A6.407,6.407,0,0,0,5.415,6.151a2.545,2.545,0,0,1-.056-.516,2.256,2.256,0,0,1,3.9-1.542,4.438,4.438,0,0,0,1.431-.544A2.248,2.248,0,0,1,9.7,4.79,4.519,4.519,0,0,0,11,4.442a4.845,4.845,0,0,1-1.13,1.165Z" transform="translate(0 -3.381)"/>--}}
-    {{--                    </svg>--}}
-    {{--                </a>--}}
-
-    {{--                <a href="{{count($behance->availableLanguage) > 0 ? $behance->availableLanguage[0]->value : ''}}" target="_blank">--}}
-    {{--                    <svg id="Group_5" data-name="Group 5" xmlns="http://www.w3.org/2000/svg" width="12.284" height="7.701" viewBox="0 0 12.284 7.701">--}}
-    {{--                        <path id="Icon_awesome-behance" data-name="Icon awesome-behance" d="M4.948,8.752A1.618,1.618,0,0,0,5.98,7.174C5.98,5.668,4.858,5.3,3.564,5.3H0V12.86H3.664c1.373,0,2.664-.659,2.664-2.195a1.821,1.821,0,0,0-1.38-1.913ZM1.661,6.592H3.22c.6,0,1.139.168,1.139.864,0,.642-.42.9-1.013.9H1.661V6.592Zm1.777,4.984H1.661V9.494H3.472c.732,0,1.194.3,1.194,1.079s-.552,1-1.228,1Zm7.646-5.133H8.019V5.7h3.065v.744Zm1.2,3.76A2.69,2.69,0,0,0,9.621,7.234a2.75,2.75,0,0,0-2.8,2.9A2.666,2.666,0,0,0,9.621,13a2.44,2.44,0,0,0,2.561-1.841H10.856a1.245,1.245,0,0,1-1.188.714,1.238,1.238,0,0,1-1.344-1.393h3.948C12.278,10.394,12.284,10.3,12.284,10.2ZM8.326,9.537A1.175,1.175,0,0,1,9.574,8.368a1.12,1.12,0,0,1,1.2,1.169Z" transform="translate(0 -5.302)"/>--}}
-    {{--                    </svg>--}}
-    {{--                </a>--}}
-
-    {{--                <a href="{{count($linkedin->availableLanguage) > 0 ? $linkedin->availableLanguage[0]->value : ''}}" target="_blank">--}}
-    {{--                    <svg id="Group_4" data-name="Group 4" xmlns="http://www.w3.org/2000/svg" width="10.326" height="10.326" viewBox="0 0 10.326 10.326">--}}
-    {{--                        <path id="Icon_awesome-linkedin-in" data-name="Icon awesome-linkedin-in" d="M2.311,10.326H.171V3.432H2.311ZM1.24,2.492A1.246,1.246,0,1,1,2.48,1.241,1.25,1.25,0,0,1,1.24,2.492Zm9.084,7.834H8.187V6.97c0-.8-.016-1.825-1.113-1.825-1.113,0-1.284.869-1.284,1.768v3.414H3.652V3.432H5.706v.94h.03A2.249,2.249,0,0,1,7.761,3.26c2.167,0,2.565,1.427,2.565,3.28v3.787Z" transform="translate(0 -0.001)"/>--}}
-    {{--                    </svg>--}}
-    {{--                      --}}
-    {{--                </a>--}}
-    {{--                <a href="{{count($instagram->availableLanguage) > 0 ? $instagram->availableLanguage[0]->value : ''}}" target="_blank">--}}
-    {{--                    <svg xmlns="http://www.w3.org/2000/svg" width="10.326" height="10.326" viewBox="0 0 64 64" aria-labelledby="title"--}}
-    {{--                         aria-describedby="desc" role="img" xmlns:xlink="http://www.w3.org/1999/xlink">--}}
-    {{--                        <path data-name="layer2"--}}
-    {{--                              d="M44.122 2H19.87A17.875 17.875 0 0 0 2 19.835v24.2a17.875 17.875 0 0 0 17.87 17.834h24.252A17.875 17.875 0 0 0 62 44.034v-24.2A17.875 17.875 0 0 0 44.122 2zM55.96 44.034a11.825 11.825 0 0 1-11.838 11.812H19.87A11.825 11.825 0 0 1 8.032 44.034v-24.2A11.825 11.825 0 0 1 19.87 8.022h24.252A11.825 11.825 0 0 1 55.96 19.835zm0 0"--}}
-    {{--                              fill="#771732" stroke="#202020" stroke-linecap="round" stroke-miterlimit="10"--}}
-    {{--                              stroke-width="2" stroke-linejoin="round"></path>--}}
-    {{--                        <path data-name="layer1" d="M32 16.45a15.484 15.484 0 1 0 15.514 15.484A15.519 15.519 0 0 0 32 16.45zm0 24.95a9.461 9.461 0 1 1 9.482-9.461A9.472 9.472 0 0 1 32 41.4zm19.263-24.834a3.719 3.719 0 1 1-3.719-3.711 3.714 3.714 0 0 1 3.719 3.711zm0 0"--}}
-    {{--                              fill="#771732" stroke="#771732" stroke-linecap="round" stroke-miterlimit="10"--}}
-    {{--                              stroke-width="2" stroke-linejoin="round"></path>--}}
-    {{--                    </svg>--}}
-    {{--                </a>--}}
-    {{--            </div>--}}
-
-    {{--        </div>--}}
-    {{--    </div>--}}
-
-
-    <div class="footer-video-modal">
-        <div class="footer-modal-wrap">
-            <button class="map-modal-close">
-                <img src="{{asset('../img/icons/svg-close-circle (1).svg')}}" alt="">
-            </button>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8426.500043046433!2d44.80081624030541!3d41.694236682267714!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440cd7e64f626b%3A0x61d084ede2576ea3!2sTbilisi%2C%20Georgia!5e0!3m2!1sen!2sus!4v1603197121014!5m2!1sen!2sus"
-                    width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-                    tabindex="0"></iframe>
+        <div class="container">
+            <div class="footer__col">
+                <div class="footer__copy" style="display: inline-flex">
+                    <h2 class="pr-5" style="margin: 4px 0">Created by</h2>
+                    <a href="https://insite.international/en/" target="_blank" style="margin: 0 10px">
+                        <img class="insite_logo" src="insite_logo.png" style="width: 80px" alt="">
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 
